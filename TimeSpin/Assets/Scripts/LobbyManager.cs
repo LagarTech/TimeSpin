2cc839ecfc0d4bbb35e14be2c446216296c6dc51
joseeeema <jmsoriano2003@gmail.com>
2024-09-21 20:55:44 +0200

    Programación Lobby 2
    Inclusión de creación de salas públicas y privadas (con código)
    Caracterización del jugador mediante personaje y nombre
    Posibilidad de unión a una sala random o a una específica con clave

--- TimeSpin/Assets/Scripts/LobbyManager.cs
+++ TimeSpin/Assets/Scripts/LobbyManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
@@ -8,7 +7,14 @@ using UnityEngine;
 
 public class LobbyManager : MonoBehaviour
 {
-    private Lobby hostLobby;
+    private Lobby _hostLobby; // Referencia a la sala creada
+    // Nombre para la sala
+    private string _lobbyName = "TimeSpin";
+    // Se almacena el número máximo de jugadores de la sala
+    private const int MAX_PLAYERS = 8;
+    // Variables encargadas de hacer una pulsación cada cierto tiempo, para que la sala no se destruya por inactividad
+    private float _heartBeatLobbyTimer = 0;
+    private const int MAX_HEARTBEAT_TIMER = 15;
 
     private async void Start()
     {
@@ -21,22 +27,162 @@ public class LobbyManager : MonoBehaviour
             Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
         };
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        CreateLobby();
+        // CreatePrivateLobby();
+        // await JoinPublicLobby();
+    }
+
+    private void Update()
+    {
+        // Si estamos unidos a un lobby, cada cierto tiempo se la mandan pulsaciones
+        if (_hostLobby != null)
+        {
+            HandleLobbyHeartbeat();
+        }
+    }
+
+    // Esta función se utiliza para enviar un mensaje al lobby cada 15 segundos, para evitar que la sala desaparezca por inactividad
+    private async void HandleLobbyHeartbeat()
+    {
+        if (_hostLobby != null)
+        {
+            _heartBeatLobbyTimer += Time.deltaTime;
+            // Si se supera el tiempo umbral sin enviar un mensaje, este se manda
+            if (_heartBeatLobbyTimer > MAX_HEARTBEAT_TIMER)
+            {
+                _heartBeatLobbyTimer = 0;
+                await LobbyService.Instance.SendHeartbeatPingAsync(_hostLobby.Id);
+            }
+        }
     }
 
 
-    // Función asíncrona para crear la sala con un nombre y un número máximo de integrantes
-    private async void CreateLobby()
+    // Función asíncrona para crear la sala privada con un nombre y un número máximo de integrantes
+    private async void CreatePrivateLobby()
     {
         try
         {
-            string lobbyName = "TestLobby";
-            int maxPlayers = 8;
-            hostLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            // Se definen las propiedades de la sala
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions
+            {
+                // La sala es privada, es decir, solo se puede unir mediante código
+                IsPrivate = true,
+                Player = SelectionController.instance.GetPlayer()
+            };
+
+            _hostLobby = await LobbyService.Instance.CreateLobbyAsync(_lobbyName, MAX_PLAYERS, createLobbyOptions);
+            Debug.Log("Created Lobby! " + _hostLobby.LobbyCode);
+            // PrintPlayers(_hostLobby);
         }
         catch (LobbyServiceException e)
         {
             Debug.Log(e);
         }
     }
+
+    // Función asíncrona para crear una sala públic
+    private async void CreatePublicLobby()
+    {
+        try
+        {
+            // Se definen las propiedades de la sala
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions
+            {
+                // La sala es privada, es decir, solo se puede unir mediante código
+                IsPrivate = false,
+                Player = SelectionController.instance.GetPlayer()
+            };
+
+            _hostLobby = await LobbyService.Instance.CreateLobbyAsync(_lobbyName, MAX_PLAYERS, createLobbyOptions);
+            Debug.Log("Created Lobby! " + _hostLobby.LobbyCode);
+            // PrintPlayers(_hostLobby);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    // Función asíncrona para unirse a una sala privada mediante código
+    private async Task JoinLobbyByCode(string lobbyCode)
+    {
+        try
+        {
+            // Se crea un jugador con las características correspondientes
+            JoinLobbyByCodeOptions options = new JoinLobbyByCodeOptions
+            {
+                Player = SelectionController.instance.GetPlayer()
+            };
+            await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode, options);
+            Debug.Log("Joined Lobby with code: " + lobbyCode);
+        }
+        catch (LobbyServiceException e)
+        {
+            // Se gestionan las excepciones que pueden ocurrir, para mostrarlas como texto en la UI
+            // En caso de que el código sea erróneo
+            if (e.Reason == LobbyExceptionReason.LobbyNotFound)
+            {
+                Debug.Log("No se ha encontrado ninguna sala con dicho código");
+            }
+            // En caso de que la sala ya tenga su máximo de 8 participantes
+            else if (e.Reason == LobbyExceptionReason.LobbyFull)
+            {
+                Debug.Log("La sala a la que se intenta acceder está llena");
+            }
+        }
+    }
+
+    // Función asíncrona para unirse a una sala pública cualquiera
+    private async Task JoinPublicLobby()
+    {
+        try
+        {
+            // Se crea un jugador con las características correspondientes
+            QuickJoinLobbyOptions options = new QuickJoinLobbyOptions
+            {
+                Player = SelectionController.instance.GetPlayer()
+            };
+            // Se intenta unir a una sala disponible
+            await LobbyService.Instance.QuickJoinLobbyAsync(options);
+        }
+        catch (LobbyServiceException e)
+        {
+            // Si no lo consigue, creará una sala pública nueva
+            Debug.Log("No hay ninguna sala activa, se creará una nueva");
+            CreatePublicLobby();
+        }
+    }
+
+    // Se obtienen los jugadores del lobby y se muestran por pantalla
+    /*
+    private void PrintPlayers(Lobby l)
+    {
+        Debug.Log("Players in lobby ------ ");
+        foreach (Player p in l.Players)
+        {
+            Debug.Log(p.Id + " " + p.Data["Name"].Value + " " + p.Data["Character"].Value);
+        }
+    }
+    */
+
+
+    // Función asíncrona para listar todas las lobbies existentes
+    /*
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach(Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    */
 }
--- TimeSpin/Assets/Scripts/SelectionController.cs
+++ TimeSpin/Assets/Scripts/SelectionController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class SelectionController : MonoBehaviour
 {
     public static SelectionController instance;
     // Variables que almacenan la información con la que se caracteriza un jugador en línea
-    private int _selectedCharacter;
-    private string _namePlayer;
+    [SerializeField] private string _selectedCharacter;
+    [SerializeField] private string _namePlayer;
 
     private void Awake()
     {
@@ -21,14 +22,30 @@ public class SelectionController : MonoBehaviour
         }
     }
     // Función para modificar el personaje escogido
-    public void ModifyCharacter(int newCharacter)
+    public void ModifyCharacter(string newCharacter)
     {
         _selectedCharacter = newCharacter;
     }
+
     // Función para cambiar el nombre del jugador
     public void ModifyName(string newName)
     {
         _namePlayer = newName;
+    }
+
+    // Función que devuelve un jugador con sus datos para incluirlo en el lobby
+    public Player GetPlayer()
+    {
+        // Este jugador se caracteriza por un nombre y un personaje seleccionado
+        return new Player
+        {
+            Data = new Dictionary<string, PlayerDataObject>
+            {
+                // Solo almacenamos el nombre del jugador, junto con el color del coche que lleve
+                { "Name", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, _namePlayer) },
+                { "Character", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, _selectedCharacter) }
+            }
+        };
     }
 
 
