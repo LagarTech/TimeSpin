964d6440371978844b5ea10fa21400b3c7172cdb
Jaco <jacox0016@gmail.com>
2024-10-06 20:16:12 +0200

    Prehistoria
    Arreglo de distancia al pegar y se pega dos veces al TRex

--- TimeSpin/Assets/Scripts/Prehistory/DinosaurController.cs
+++ TimeSpin/Assets/Scripts/Prehistory/DinosaurController.cs
@@ -5,6 +5,28 @@ public class DinosaurController : MonoBehaviour
     public int points;               // Puntos del dinosaurio
     public float visibleTime = 1.5f; // Tiempo que estará visible
     private float timer;
+    public string dinosaurType;         // Tipo de dinosaurio (Normal, Velociraptor, TRex)
+
+    public Transform player;         // Referencia al jugador
+    public float hitDistance = 2.0f; // Distancia mínima para golpear al dinosaurio
+
+    private int hitCount = 0;           // Cuenta los clicks (para el T-Rex)
+    public int requiredHits = 1;        // Golpes minimos para matar al dinosaurio (1 por defecto)
+
+    void Start()
+    {
+        // Si el dinosaurio es un T-Rex, necesita dos golpes
+        if (dinosaurType == "TRex")
+        {
+            requiredHits = 2;
+        }
+
+        // Encontramos al jugador en la escena, por si no lo hemos asignado manualmente en el inspector
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
 
     void Update()
     {
@@ -17,11 +39,37 @@ public class DinosaurController : MonoBehaviour
 
     void OnMouseDown()
     {
-        // Al hacer clic sobre el dinosaurio
-        GameObject gameManager = GameObject.Find("GameManager");
-        gameManager.GetComponent<GameManager>().AddScore(points);
+        // Comprobar si el jugador esta cerca para pincharle
+        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
 
-        Destroy(gameObject);  // Elimina el dinosaurio después de pulsarlo
+        if (distanceToPlayer <= hitDistance)
+        {
+            // El jugador esta cerca, lo golpea 
+            HitDinosaur();
+        }
+        else
+        {
+            // el jugador esta lejos
+            Debug.Log("Estás demasiado lejos para golpear el dinosaurio.");
+        }
+    }
+
+    void HitDinosaur()
+    {
+        hitCount++;
+
+        if (hitCount >= requiredHits)
+        {
+            // Se suman los puntos al pinchar el dinosaurio
+            GameObject gameManager = GameObject.Find("GameManager");
+            gameManager.GetComponent<GameManager>().AddScore(points);
+
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("El TRex ha sido golpeado, pero aún necesita más golpes.");
+        }
     }
 }
 
